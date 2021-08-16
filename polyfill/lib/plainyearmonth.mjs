0af998ced5a4b2ff7734b489a559574d56113976
Philip Chimento <pchimento@igalia.com>
2021-08-16 13:08:54 -0700

    Normative: Consistent order of operations in PMD.toPD and PYM.toPD
    The spec text was inconsistent about whether the type check on the
    argument or the calendar.fields checks should occur first. In
    Temporal.PlainMonthDay.prototype.toPlainDate, the fields checks were
    first. In Temporal.PlainYearMonth.prototype.toPlainDate, the type check
    was first. This changes the PlainMonthDay algorithm to match the
    PlainYearMonth one, and adds tests to both.

--- polyfill/lib/plainyearmonth.mjs
+++ polyfill/lib/plainyearmonth.mjs
@@ -295,6 +295,7 @@ export class PlainYearMonth {
   }
   toPlainDate(item) {
     if (!ES.IsTemporalYearMonth(this)) throw new TypeError('invalid receiver');
+    if (ES.Type(item) !== 'Object') throw new TypeError('argument should be an object');
     const calendar = GetSlot(this, CALENDAR);
 
     const receiverFieldNames = ES.CalendarFields(calendar, ['monthCode', 'year']);
